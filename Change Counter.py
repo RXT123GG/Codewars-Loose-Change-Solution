49ac5c1adb621702ae8df8737bff8fc2b71c61cd
Remy Taylor <116510743+RXT123GG@users.noreply.github.com>
2025-09-28 20:10:20 -0400

    Add files via upload

---
+++ Change Counter.py
@@ -0,0 +1,23 @@
+#Codewars "Loose Change!" solution.
+# Defines a function that takes a string of change-inputs, seperates them into a list, sorts them by type and adds to a counter
+# then returns a formatted output in $0.00 format as a string
+
+
+def change_count(change):
+    changeList = change.split()
+    moneyCounter = 0.00
+    for change in changeList:
+        if change == "penny":
+            moneyCounter += 0.01
+        elif change == "nickel":
+            moneyCounter += 0.05
+        elif change == "dime":
+            moneyCounter += 0.10
+        elif change == "quarter":
+            moneyCounter += 0.25
+        elif change == "dollar":
+            moneyCounter += 1.00
+        else:
+            print(str(change))
+                     
+    return(f"${moneyCounter:.2f}")
